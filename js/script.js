21965776e94c4b36081a33d8b92119fb21847772
Raisson A. Santana <almeidaraisson@gmail.com>
2023-03-19 18:18:57 -0300

    new layout with shadow navbar and toast

--- js/script.js
+++ js/script.js
@@ -1,145 +1,133 @@
 var round = 0,
-    roboScore = 0,
-    playerScore = 0;
+  roboScore = 0,
+  playerScore = 0;
 
-window.addEventListener('load', () => {
-    addEvent();
+window.addEventListener("load", () => {
+  addEvent();
 });
 
-function addEvent(){
-    $("#btnStone").bind('click', buttonClick);
-    $("#btnPapper").bind('click', buttonClick);
-    $("#btnSciss").bind('click', buttonClick);
-    $("#btnPlay").bind('click', buttonClick);
+function addEvent() {
+  $("#btnStone").bind("click", buttonClick);
+  $("#btnPapper").bind("click", buttonClick);
+  $("#btnSciss").bind("click", buttonClick);
+  $("#btnPlay").bind("click", buttonClick);
 }
 
 function getRandom() {
-    let min = 1,
-        max = 3;
-    let retult = Math.floor(Math.random() * (max-min+1) + min);
+  let min = 1,
+    max = 3;
+  let retult = Math.floor(Math.random() * (max - min + 1) + min);
 
-    return retult;
+  return retult;
 }
 
-function buttonClick(event){
-    let currentClick = event.target.id;
-    let choice = 0,
-        robo = null;
-    
-        robo = getRandom();
+function buttonClick(event) {
+  let currentClick = event.target.id;
+  let choice = 0,
+    robo = null;
 
-        switch(currentClick) {
-            case 'btnStone':
-                choice = 1;
-                gaming(choice, robo);
-                break;
-                case 'btnPapper':
-                    choice = 2;
-                    gaming(choice, robo);
-                    break;
-                    case 'btnSciss':
-                        choice = 3;
-                        gaming(choice, robo);
-                break;
-        }   
+  robo = getRandom();
+
+  switch (currentClick) {
+    case "btnStone":
+      choice = 1;
+      gaming(choice, robo);
+      break;
+    case "btnPapper":
+      choice = 2;
+      gaming(choice, robo);
+      break;
+    case "btnSciss":
+      choice = 3;
+      gaming(choice, robo);
+      break;
+  }
 }
 
+function gaming(player, robo) {
+  var msg = "";
 
-function gaming(player, robo){
-    var msg = '';
-
-    switch(player){
-        case 1: {
-            if(robo == 2){
-                msg = "Você perdeu!";
-                roboScore++;
-                break;
-            }
-            else if(robo == 3){
-                msg = "Você ganhou!";
-                playerScore++;
-                break;
-            }
-            else{
-                msg = "Empatou!";
-                break;
-            }
-        }
-        case 2: {
-            if(robo == 3){
-                msg = "Você perdeu!";
-                roboScore++;
-                break;
-            }
-            else if(robo == 1){
-                msg = "Você ganhou!";
-                playerScore++;
-                break;
-            }
-            else{
-                msg = "Empatou!";
-                break;
-            }
-        }
-        case 3: {
-            if(robo == 1){
-                msg = "Você perdeu!";
-                roboScore++;
-                break;
-            }
-            else if(robo == 2){
-                msg = "Você ganhou!";
-                playerScore++;
-                break;
-            }
-            else{
-                msg = "Empatou!";
-                break;
-            }
-        }
+  switch (player) {
+    case 1: {
+      if (robo == 2) {
+        msg = "Você perdeu!";
+        roboScore++;
+        break;
+      } else if (robo == 3) {
+        msg = "Você ganhou!";
+        playerScore++;
+        break;
+      } else {
+        msg = "Empatou!";
+        break;
+      }
     }
-    
-    round++;
-    render(robo, msg);
+    case 2: {
+      if (robo == 3) {
+        msg = "Você perdeu!";
+        roboScore++;
+        break;
+      } else if (robo == 1) {
+        msg = "Você ganhou!";
+        playerScore++;
+        break;
+      } else {
+        msg = "Empatou!";
+        break;
+      }
+    }
+    case 3: {
+      if (robo == 1) {
+        msg = "Você perdeu!";
+        roboScore++;
+        break;
+      } else if (robo == 2) {
+        msg = "Você ganhou!";
+        playerScore++;
+        break;
+      } else {
+        msg = "Empatou!";
+        break;
+      }
+    }
+  }
+
+  round++;
+  render(robo, msg);
 }
 
-function render(robo, msg){
-
-    switch(robo){
-        case 1:{
-            img = 
-            `
+function render(robo, msg) {
+  switch (robo) {
+    case 1: {
+      img = `
             <div class="card">
                 <input id="btnStone" class="button btn btn-outline-dark" src="./imgs/pedra.png" alt="punhofechado" type="image">
                 <p><strong>Pedra</strong></p>
             </div>
-            `
-            break;
-        }
-        case 2:{
-            img = 
-            `
+            `;
+      break;
+    }
+    case 2: {
+      img = `
             <div class="card">
                 <input id="btnPapper" class="button btn btn-outline-warning" src="./imgs/papel.png" alt="mão aberta" type="image">
                 <p><strong>Papel</strong></p>
             </div>           
-            `
-            break;
-        }
-        case 3:{
-            img = 
-            `
+            `;
+      break;
+    }
+    case 3: {
+      img = `
             <div class="card">
                 <input id="btnSciss" class="button btn btn-outline-danger" src="./imgs/tesoura.png" alt="dedos abertos em tesoura" type="image">
                 <p><strong>Tesoura</strong></p>
             </div>
-            `
-            break;
-        }
-        
+            `;
+      break;
     }
+  }
 
-    $("#divResult").html(`
+  $("#divResult").html(`
 
     <span class = "fs-4"><strong>${msg}</strong></span>
     <div class="card-body">
@@ -159,28 +147,52 @@ function render(robo, msg){
     </ul>
     `);
 
-    $("#divPlacar").html(
-        `
+  $("#divPlacar").html(
+    `
         <ul>
-          <li id="player" class="liPlayer">PLAYER: ${playerScore}</li>
-          <li id="robo" class="liRobo">ROBÔ: ${roboScore}</li>
+        <li id="player" class="liPlayer">PLAYER: ${playerScore}</li>
+        <li id="robo" class="liRobo">ROBÔ: ${roboScore}</li>
         </ul>
         `
-    )
+  );
 
-    
+  toast(msg);
 }
 
+// -------------------TOAST-----------------------
+function toast(msg) {
+  const toastLiveExample = document.getElementById("liveToast");
+  const toast = new bootstrap.Toast(toastLiveExample);
 
-// ----------------NAVBAR-------------------------
+  $("#toastMsg").html(
+    `
+        <span>${msg}</span>
+        `
+  );
 
-$(document).ready(function() {
-    $(window).scroll(function() {
-      if($(this).scrollTop() > 0){
-          $(".navbar").addClass("navActive");
+  toast.show();
+}
+
+// ---------------- NAVBAR -------------------------
+
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 0) {
+      $(".navbar").addClass("navActive");
+    } else {
+      $(".navbar").removeClass("navActive");
     }
-    else{
-          $(".navbar").removeClass("navActive");
-      }
-    });
   });
+});
+
+// ---------------- TOAST -------------------------
+
+const toastTrigger = document.getElementById("liveToastBtn");
+const toastLiveExample = document.getElementById("liveToast");
+if (toastTrigger) {
+  toastTrigger.addEventListener("click", () => {
+    const toast = new bootstrap.Toast(toastLiveExample);
+
+    toast.show();
+  });
+}
