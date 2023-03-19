016858ce764fd27cf94a3039246c57c0d527deee
Raisson A. Santana <almeidaraisson@gmail.com>
2023-03-19 20:00:06 -0300

    add loader

--- js/script.js
+++ js/script.js
@@ -11,11 +11,26 @@ function addEvent() {
   $("#btnPapper").bind("click", buttonClick);
   $("#btnSciss").bind("click", buttonClick);
   $("#btnPlay").bind("click", buttonClick);
+  $("#btnLogo").bind("click", btnLogoPlay);
+}
+
+function btnLogoPlay(){
+    let choice = getRandom();
+    let robo = getRandom();
+
+    // $("#divResult").html('');
+    $("#loader").removeClass("loaderOff");
+    $("#loader").addClass("loaderActive");
+    setTimeout(function(){
+        
+        $(".loader").removeClass("loaderActive");
+        gaming(choice, robo);
+       
+    }, 1000);  
 }
 
 function getRandom() {
-  let min = 1,
-    max = 3;
+  let min = 1, max = 3;
   let retult = Math.floor(Math.random() * (max - min + 1) + min);
 
   return retult;
@@ -50,11 +65,11 @@ function gaming(player, robo) {
   switch (player) {
     case 1: {
       if (robo == 2) {
-        msg = "Você perdeu!";
+        msg = "Você perdeu! ;(";
         roboScore++;
         break;
       } else if (robo == 3) {
-        msg = "Você ganhou!";
+        msg = "Você ganhou! :)";
         playerScore++;
         break;
       } else {
@@ -64,11 +79,11 @@ function gaming(player, robo) {
     }
     case 2: {
       if (robo == 3) {
-        msg = "Você perdeu!";
+        msg = "Você perdeu! ;(";
         roboScore++;
         break;
       } else if (robo == 1) {
-        msg = "Você ganhou!";
+        msg = "Você ganhou! :)";
         playerScore++;
         break;
       } else {
@@ -78,11 +93,11 @@ function gaming(player, robo) {
     }
     case 3: {
       if (robo == 1) {
-        msg = "Você perdeu!";
+        msg = "Você perdeu! ;(";
         roboScore++;
         break;
       } else if (robo == 2) {
-        msg = "Você ganhou!";
+        msg = "Você ganhou! :)";
         playerScore++;
         break;
       } else {
@@ -102,8 +117,8 @@ function render(robo, msg) {
   switch (robo) {
     case 1: {
       img = `
-            <div class="card">
-                <input id="btnStone" class="button btn btn-outline-dark" src="./imgs/pedra.png" alt="punhofechado" type="image">
+            <div class="card rounded-4">
+                <input id="btnStone" class="button btn btn-outline-dark rounded-4" src="./imgs/pedra.png" alt="punhofechado" type="image">
                 <p><strong>Pedra</strong></p>
             </div>
             `;
@@ -111,8 +126,8 @@ function render(robo, msg) {
     }
     case 2: {
       img = `
-            <div class="card">
-                <input id="btnPapper" class="button btn btn-outline-warning" src="./imgs/papel.png" alt="mão aberta" type="image">
+            <div class="card rounded-4">
+                <input id="btnPapper" class="button btn btn-outline-warning rounded-4" src="./imgs/papel.png" alt="mão aberta" type="image">
                 <p><strong>Papel</strong></p>
             </div>           
             `;
@@ -120,8 +135,8 @@ function render(robo, msg) {
     }
     case 3: {
       img = `
-            <div class="card">
-                <input id="btnSciss" class="button btn btn-outline-danger" src="./imgs/tesoura.png" alt="dedos abertos em tesoura" type="image">
+            <div class="card rounded-4">
+                <input id="btnSciss" class="button btn btn-outline-danger rounded-4" src="./imgs/tesoura.png" alt="dedos abertos em tesoura" type="image">
                 <p><strong>Tesoura</strong></p>
             </div>
             `;
